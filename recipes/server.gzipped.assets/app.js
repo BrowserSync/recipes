01f627775ed6651e584f6b2321dd94d149fce6d9
Shane Osbourne <shane@wearejh.com>
2015-06-01 14:26:47 +0100

    Update gzip example to catch all files: re: #4

--- recipes/server.gzipped.assets/app.js
+++ recipes/server.gzipped.assets/app.js
@@ -1,13 +1,19 @@
 /**
  * Require Browsersync
  */
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
+var middleware  = require('connect-gzip-static')('./app');
 
 /**
  * Run Browsersync with server config
+ * Add middleware with override:true to ensure all files are
+ * picked up.
  */
-browserSync({
-    server: "app",
-    files: ["app/*.html", "app/css/*.css"],
-    middleware: require("connect-gzip-static")("./app")
+browserSync.init({
+    server: 'app',
+    files: ['app/*.html', 'app/css/*.css']
+}, function (err, bs) {
+    bs.addMiddleware("*", middleware, {
+        override: true
+    });
 });
