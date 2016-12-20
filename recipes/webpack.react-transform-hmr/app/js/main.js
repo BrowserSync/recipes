df233a9d049456818b9f96d2c80f662821b60104
Peter Krieg <pkrieg2@gmail.com>
2016-12-20 00:20:33 -0500

    update deps for "webpack.react-transform-hmr" recipe

--- recipes/webpack.react-transform-hmr/app/js/main.js
+++ recipes/webpack.react-transform-hmr/app/js/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { render } from 'react-dom';
 // It's important to not define HelloWorld component right in this file
 // because in that case it will do full page reload on change
 import HelloWorld from './HelloWorld.jsx';
 
-React.render(<HelloWorld />, document.getElementById('react-root'));
+render(<HelloWorld />, document.getElementById('react-root'));
